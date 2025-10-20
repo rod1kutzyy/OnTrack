9568d36e025ad2765dd109de82bce4cdd003ba62
Rodion Agutin <rodionagutin@gmail.com>
2025-10-20 12:13:05 +0300

    Handle custom errors in todo repository

--- backend/internal/repository/todo_repository.go
+++ backend/internal/repository/todo_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/rod1kutzyy/OnTrack/internal/entity"
 	"github.com/rod1kutzyy/OnTrack/internal/usecase"
@@ -17,25 +18,51 @@ func NewTodoRepository(db *gorm.DB) usecase.TodoRepository {
 }
 
 func (r *todoRepository) Create(ctx context.Context, todo *entity.Todo) error {
-	return r.db.WithContext(ctx).Create(todo).Error
+	if err := r.db.WithContext(ctx).Create(todo).Error; err != nil {
+		return entity.ErrTodoDatabase
+	}
+	return nil
 }
 
 func (r *todoRepository) GetAll(ctx context.Context) ([]entity.Todo, error) {
 	var todos []entity.Todo
 	err := r.db.WithContext(ctx).Find(&todos).Error
-	return todos, err
+	if err != nil {
+		return nil, entity.ErrTodoDatabase
+	}
+	return todos, nil
 }
 
 func (r *todoRepository) GetByID(ctx context.Context, id uint) (*entity.Todo, error) {
 	var todo entity.Todo
 	err := r.db.WithContext(ctx).First(&todo).Error
-	return &todo, err
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, entity.ErrTodoNotFound
+		}
+		return nil, entity.ErrTodoDatabase
+	}
+	return &todo, nil
 }
 
 func (r *todoRepository) Update(ctx context.Context, todo *entity.Todo) error {
-	return r.db.WithContext(ctx).Save(todo).Error
+	err := r.db.WithContext(ctx).Save(todo).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return entity.ErrTodoNotFound
+		}
+		return entity.ErrTodoDatabase
+	}
+	return nil
 }
 
 func (r *todoRepository) Delete(ctx context.Context, id uint) error {
-	return r.db.WithContext(ctx).Delete(&entity.Todo{}, id).Error
+	result := r.db.WithContext(ctx).Delete(&entity.Todo{}, id)
+	if result.Error != nil {
+		return entity.ErrTodoDatabase
+	}
+	if result.RowsAffected == 0 {
+		return entity.ErrTodoNotFound
+	}
+	return nil
 }
